9d20978064fcfd5c995cf4c9678896e21bdb6108
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-07 20:46:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 0
   square_array = []
   while array[count] do
-    square_array.push(array[count])
+    square_array.push(array[count]**2)
     counter += 1
   end
   square_array
