bb6b2d88a0b169aa9bc48fd67c2ae3d5420090ac
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-07 20:56:43 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,14 @@
+array = [1,2,3,4]
+
+def square_array(array)
+  count = 0
+  square_array = []
+  while count < array.length do
+    # square_array.push(array[count]**2)
+    square_array.push(array[count])
+    count += 1
+  end
+  # square_array
+end
+
+puts square_array(array)
