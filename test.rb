e4187dc1a2ce4c649990693e3d7635bfec5faf95
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-07 20:49:56 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -2,14 +2,13 @@ array = [1,2,3,4]
 
 def square_array(array)
   count = 0
-  # square_array = []
+  square_array = []
   while array[count] do
     # square_array.push(array[count]**2)
-    puts array[count]
+    puts square_array.push(array[count])
     counter += 1
   end
   # square_array
 end
 
 # puts square_array
-puts array
